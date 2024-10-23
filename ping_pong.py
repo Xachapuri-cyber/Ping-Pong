8b351f338f3309a180d540a039f8110a041ba2bc
Xachapuri-cyber <Superxachapuri@gmail.com>
2024-10-23 17:38:30 +0300

    Update ping_pong.py
    Финальное обновление кода

--- ping_pong.py
+++ ping_pong.py
@@ -7,10 +7,16 @@ mixer.init()
 win_width = 700
 win_heidth = 500
 
+font1 = font.SysFont("Arial", 35)
+lose1 = font1.render("Игрок 1 проиграл!", True, (180, 0, 0))
+lose2 = font1.render("Игрок 2 проиграл!", True, (180, 0, 0))
+
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, player_width, player_height):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (player_width, player_height))
+        self.player_width = player_width
+        self.player_height = player_height
+        self.image = transform.scale(image.load(player_image), (self.player_width, self.player_height))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -34,9 +40,17 @@ class Player(GameSprite):
             self.rect.y += self.speed
 
 class Ball(GameSprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, player_width, player_height, speed_x, speed_y):
+        super().__init__(player_image, player_x, player_y, player_speed, player_width, player_height)
+        self.speed_x = speed_x
+        self.speed_y = speed_y
     def update(self):
-        self.rect.x += self.speed
-        self.rect.y -= self.speed
+        self.rect.x += self.speed_x
+        self.rect.y -= self.speed_y
+        if self.rect.y > win_heidth - self.player_height or self.rect.y < 0:
+            self.speed_y *= -1
+        if sprite.collide_rect(player1, self) or sprite.collide_rect(player2, self):
+            self.speed_x *= -1
 
 window = display.set_mode((win_width, win_heidth))
 display.set_caption("Пинг понг")
@@ -49,7 +63,7 @@ finish = False
 
 player1 = Player("racket.png", 620, 250, 5, 65, 125)
 player2 = Player("racket.png", 15, 250, 5, 65, 125)
-ball = Ball("ball.png", 350, 250, 4, 40, 40)
+ball = Ball("ball.png", 350, 250, 0, 40, 40, 5, 4)
 background = transform.scale(image.load("background.jpg"), (win_width, win_heidth))
 # mixer.music.load('space.ogg')
 # mixer.music.set_volume(0.1)
@@ -61,14 +75,21 @@ while game:
         if e.type == QUIT:
             game = False
     if finish != True:
-            
-            window.blit(background,(0,0))
-            player1.reset()
-            player1.update_r()
-            player2.reset()
-            player2.update_l()
-            ball.reset()
-            ball.update()
-            
+        
+        window.blit(background,(0,0))
+        player1.reset()
+        player1.update_r()
+        player2.reset()
+        player2.update_l()
+        ball.reset()
+        ball.update()
+        if ball.rect.x < -50:
+            finish = True
+            window.blit(lose1, (200, 200))
+        if ball.rect.x > 750:
+            finish = True
+            window.blit(lose2, (200, 200))
+        
+
     display.update()
     clock.tick(FPS)
